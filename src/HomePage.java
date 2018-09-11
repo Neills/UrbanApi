5546655b50dc333c1313a637e28dd80429a54615
Dar9586 <atoppi2012@libero.it>
2018-09-11 14:22:08 +0200

    Added getter Improved performance

--- src/HomePage.java
+++ src/HomePage.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 
 public class HomePage {
 
-    private String date;
-    private String word;
-    private String meaning;
-    private String example;
-    private String tags[];
-    private String contributor;
-    private int like;
-    private int dislike;
+    private final String date;
+    private final String word;
+    private final String meaning;
+    private final String example;
+    private final String tags[];
+    private final String contributor;
+    private final int like;
+    private final int dislike;
 
     public HomePage(Element e){
         date=e.getElementsByClass("ribbon").first().text();
@@ -24,23 +24,61 @@ public class HomePage {
             e1 = e1.first().getElementsByTag("a");
             tags = new String[e1.size()];
             for (int i = 0; i < e1.size(); i++) {
-                tags[i] = e1.get(i).text();
+                tags[i] = e1.get(i).text().substring(1);
             }
         }
         contributor=e.getElementsByClass("contributor").first().text();
-        like=Integer.parseInt(e.getElementsByClass("count").get(0).text());
-        dislike=Integer.parseInt(e.getElementsByClass("count").get(1).text());
-        System.out.println(toString());
+        e1=e.getElementsByClass("count");
+        like=Integer.parseInt(e1.get(0).text());
+        dislike=Integer.parseInt(e1.get(1).text());
     }
 
-    @Override
-    public String toString() {
+    public String getDate() {
+        return date;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public String getExample() {
+        return example;
+    }
+
+    public String getContributor() {
+        return contributor;
+    }
+
+    public int getLike() {
+        return like;
+    }
+
+    public int getDislike() {
+        return dislike;
+    }
+
+    public int getTagCount(){
+        return tags.length;
+    }
+
+    public String getTag(int i){
+        return tags[i];
+    }
+
+
+
+    @java.lang.Override
+    public java.lang.String toString() {
         return "HomePage{" +
                 "date='" + date + '\'' +
                 ", word='" + word + '\'' +
                 ", meaning='" + meaning + '\'' +
                 ", example='" + example + '\'' +
-                ", tags=" + Arrays.toString(tags) +
+                ", tags=" + java.util.Arrays.toString(tags) +
                 ", contributor='" + contributor + '\'' +
                 ", like=" + like +
                 ", dislike=" + dislike +
